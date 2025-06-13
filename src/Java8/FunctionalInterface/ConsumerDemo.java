c9627f9e1f38553d2e8222e04378f73471fba048
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-06-14 04:48:55 +0530

    ConsumerDemo

--- src/Java8/FunctionalInterface/ConsumerDemo.java
+++ src/Java8/FunctionalInterface/ConsumerDemo.java
@@ -1,11 +1,16 @@
 package Java8.FunctionalInterface;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class ConsumerDemo {
     public static void main(String[] args) {
         Consumer<Integer> consumer = t -> System.out.println("Just consumed "+t);
         consumer.accept(10);
+
+        List<Integer> list = Arrays.asList(1,2,55,6,7);
+        list.stream().forEachOrdered(t -> System.out.println("Just consumed "+t));
     }
 
 }
