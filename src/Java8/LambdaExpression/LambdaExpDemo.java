0be8b10c280e40fcf35b8882342e683ad4864dc5
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-06-14 04:06:02 +0530

    Basic Lambda Expression

---
+++ src/Java8/LambdaExpression/LambdaExpDemo.java
@@ -0,0 +1,15 @@
+package Java8.LambdaExpression;
+
+public class LambdaExpDemo {
+
+    public static void m1(){
+        System.out.println("This is a normal Method");
+    }
+
+
+    public static void main(String[] args) {
+        m1();
+        Runnable runnable = () -> System.out.println("This is a Lambda Expression");
+        runnable.run();
+    }
+}
