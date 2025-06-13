5bcbd22b001a84af552177428ff813d13c428d31
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-06-14 04:21:51 +0530

    Lambda exp to sum

--- src/Java8/Lambda/Calculator.java
+++ src/Java8/Lambda/Calculator.java
@@ -1,11 +1,15 @@
 package Java8.Lambda;
 interface Calci{
-    void switchOn();
+    //void switchOn();
+    int sum(int a, int b);
 }
 public class Calculator {
     public static void main(String[] args) {
-        Calci calci = () -> System.out.println("Calci Switched on"); //lamda expression to overide to switchon
-
-        calci.switchOn();
+//        Calci calci = () -> System.out.println("Calci Switched on"); //lamda expression to overide to switchon
+//
+//        calci.switchOn();
+        int a =10, b= 20;
+        Calci calci = (x,y) -> x+y;
+        System.out.println(calci.sum(a,b));
     }
 }
