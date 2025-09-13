4d964006853a7a292fff5da2b7bf73f9f0114d30
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-09-13 18:44:41 +0530

    Selection Sort

--- src/DSA/Arrays/selectionSort.java
+++ src/DSA/Arrays/selectionSort.java
@@ -5,30 +5,36 @@ import java.util.Scanner;
 
 public class selectionSort {
     static void selectionSort(int[] arr){
-        int temp = 0;
-        for(int i=0;i<arr.length;i++){
-            int minIdx =i;
+        for(int i = 0; i < arr.length - 1; i++){
+            int minIdx = i;
 
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]>arr[j]){
+            // Find the minimum element in remaining unsorted array
+            for(int j = i + 1; j < arr.length; j++){
+                if(arr[j] < arr[minIdx]){  // Compare with arr[minIdx], not arr[i]
                     minIdx = j;
                 }
-                temp = arr[i];
-                arr[i] = arr[minIdx];
-                arr[minIdx] = temp;
-
             }
+
+            // Swap the found minimum element with the first element
+            // This should be OUTSIDE the inner loop
+            int temp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = temp;
         }
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
-        for(int i=0;i<n;i++){
+        for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
         }
-        selectionSort(arr);
-        System.out.println(Arrays.toString(arr));
 
+        System.out.println("Original array: " + Arrays.toString(arr));
+        selectionSort(arr);
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+
+        sc.close();
     }
 }
